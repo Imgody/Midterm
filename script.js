10bbc24c5041b0d46231c983320bcb41b1f85c80
Imgody <jessealexandergody@gmail.com>
2023-05-12 19:32:35 -0700

    Midterm

---
+++ script.js
@@ -0,0 +1,42 @@
+window.addEventListener('load', function() {
+    var slider = document.querySelector('.slider');
+    var slides = slider.getElementsByClassName('slide');
+    var currentSlide = 0;
+  
+    function showSlide(n) {
+      for (var i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('active');
+      }
+    
+      slides[n].classList.add('active');
+      currentSlide = n;
+    }
+  
+    function nextSlide() {
+      currentSlide++;
+      if (currentSlide >= slides.length) {
+        currentSlide = 0;
+      }
+      showSlide(currentSlide);
+    }
+  
+    function prevSlide() {
+      currentSlide--;
+      if (currentSlide < 0) {
+        currentSlide = slides.length - 1;
+      }
+      showSlide(currentSlide);
+    }
+  
+    // Automatic slideshow
+    setInterval(nextSlide, 5000); // Change slide every 5 seconds
+  
+    // Event listeners for prev/next buttons
+    var prevButton = document.getElementById('prev');
+    var nextButton = document.getElementById('next');
+  
+    prevButton.addEventListener('click', prevSlide);
+    nextButton.addEventListener('click', nextSlide);
+  });
+  
+  
